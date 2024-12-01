bc9c03ca17f5a4f1048735bffcae0ec3b71a2bc3
Gideon Yuval <gideonyuval@gmail.com>
2024-12-01 18:46:05 +0200

    added follow comments

--- 9165/Program.cs
+++ 9165/Program.cs
@@ -10,6 +10,8 @@ namespace _9165
     {
         static void Main(string[] args)
         {
+            //follow What1, What2 and What for ->4->2->3
+            //follow What 4 for ->E->D->C
         }
 
         static int What1(Node<int> lst)
